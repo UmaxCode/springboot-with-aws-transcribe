3695c90436fb9a8180338e5e78cd41f1dd086f41
umaxcode <maxwellodoom1729@gmail.com>
2025-03-12 16:14:52 +0000

    chore: add initial project setup

---
+++ src/main/java/com/umaxcode/springboot_with_aws_transcribe/SpringbootWithAwsTranscribeApplication.java
@@ -0,0 +1,13 @@
+package com.umaxcode.springboot_with_aws_transcribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithAwsTranscribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithAwsTranscribeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umaxcode/springboot_with_aws_transcribe/SpringbootWithAwsTranscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umaxcode.springboot_with_aws_transcribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithAwsTranscribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
