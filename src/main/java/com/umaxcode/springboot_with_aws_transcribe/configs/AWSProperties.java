b4ea2f237e2d2af1eb5aa336e7270292de4c71fd
umaxcode <maxwellodoom1729@gmail.com>
2025-03-13 04:44:08 +0000

    feat: add bean configuration for aws s3 and transcribe

---
+++ src/main/java/com/umaxcode/springboot_with_aws_transcribe/configs/AWSProperties.java
@@ -0,0 +1,17 @@
+package com.umaxcode.springboot_with_aws_transcribe.configs;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "application.aws")
+public class AWSProperties {
+    private String region;
+    private String accessKey;
+    private String secretKey;
+}
---
+++ src/main/java/com/umaxcode/springboot_with_aws_transcribe/configs/AppConfig.java
@@ -0,0 +1,38 @@
+package com.umaxcode.springboot_with_aws_transcribe.configs;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.transcribe.AmazonTranscribe;
+import com.amazonaws.services.transcribe.AmazonTranscribeClientBuilder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class AppConfig {
+
+    private final AWSProperties properties;
+
+    @Bean
+    public AmazonTranscribe transcribeClient() {
+        log.debug("Initialize Transcribe Client");
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(properties.getAccessKey(), properties.getSecretKey());
+        AWSStaticCredentialsProvider awsStaticCredentialsProvider = new AWSStaticCredentialsProvider(awsCredentials);
+        return AmazonTranscribeClientBuilder.standard().withCredentials(awsStaticCredentialsProvider)
+                .withRegion(properties.getRegion()).build();
+    }
+
+    @Bean
+    public AmazonS3 s3Client() {
+        log.debug("Initialize AWS S3 Client");
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(properties.getAccessKey(), properties.getSecretKey());
+        AWSStaticCredentialsProvider awsStaticCredentialsProvider = new AWSStaticCredentialsProvider(awsCredentials);
+        return AmazonS3ClientBuilder.standard().withCredentials(awsStaticCredentialsProvider).withRegion(properties.getRegion())
+                .build();
+    }
+}
