cdd79b2a8520365794822ed80e5027b8256b52e2
umaxcode <maxwellodoom1729@gmail.com>
2025-03-13 09:30:51 +0000

    feat: enable scheduling in the project and add log to track scheduling

--- src/main/java/com/umaxcode/springboot_with_aws_transcribe/configs/AppConfig.java
+++ src/main/java/com/umaxcode/springboot_with_aws_transcribe/configs/AppConfig.java
@@ -10,7 +10,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
